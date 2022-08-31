3f8728d0943e7450ac6b62a6375d739a233a6708
Rodion <drugoklasnik@gmail.com>
2022-08-31 17:42:03 +0300

    2 components and vue 3

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="center">
-			<div class="wrapper" id="tasklist">
-		<h1 class="todolist_header">ToDo List</h1>
+		<div class="wrapper" id="tasklist">  
+			<h1 class="todolist_header">ToDo List</h1>
 		<div v-if="!isEditing" class="todolist_subheader">
 			<input class="todolist_placeholder" type="text" placeholder="Add TO DO" v-model="todo.itemName" >
 			<button class="todolist_btn add" value="add" @click="addTodo">ADD</button>
@@ -29,13 +29,25 @@
 			</li>
 		</ul> -->
 		<!-- <h1 v-for="todo in todos" :key="todo">{{todo}}</h1> -->
-
-		<div class="todolist_content" v-for="(todo, index) in todos" :key="todo">
-			<p class="todolist_content_text" v-bind:class="{strikethrough:todo.status==='done'}">{{todo.itemName}}</p>
-			<button class="todolist_content_btn edit" @click="editTodo(index, todo)" >EDIT</button>
-			<button class="todolist_content_btn done" @click="done(index, todo)">DONE</button>
-			<button class="todolist_content_btn delete" @click="deleteTodo(index)">DELETE</button>	
-		</div>
+		<TodoContent 
+		v-for="(todo, index) in todos"  
+		:key="index" 
+		:todo="todo"
+		:index="index"
+		@edit="editTodo"
+		@delete="deleteTodo"
+		/>
+		<!-- 
+		<TodoContent 
+		v-for="(todo, index) in todos"  
+		:key="index" 
+		:todo="todo"
+		:index="index"
+		@edit="editTodo"
+		@delete="deleteTodo"
+		/> 
+		-->
+		<!-- @done="done" -->
 	</div>
 	</div>
 </template>
@@ -46,16 +58,29 @@
 
 <script>
 import { ref } from '@vue/reactivity'
+import TodoContent from './TodoContent.vue'
 export default {
   	name: 'To do list',
+	components: {TodoContent},
 	setup(){
 		const isEditing = ref(false)
 		const selectedIndex = ref(null)
 		const todo = ref({
+
 			itemName: '',
-			dateTimeUtc: ''
+			dateTimeUtc: '',
+			isStrikeThrough: false
 		})
 		const todos = ref([])
+		function deleteTodo(index) {
+			todos.value.splice(index, 1)
+		}
+		function editTodo(index, item) {
+			todo.value = item
+			selectedIndex.value = index
+			isEditing.value = true
+			console.log(index, item)
+		}
 		function addTodo() {
 			if (todo.value.itemName.length === 0) {
 				return
@@ -71,33 +96,22 @@ export default {
   			// fetch("https://item-management.azurewebsites.net/api/items", requestOptions)
 			todo.value.itemName = ''
 		}
-		function editTodo(index, item) {
-			todo.value = item
-			selectedIndex.value = index
-			isEditing.value = true
-		} 
-		function done(index, todo) {
-			todo.status='done'
-		}
+		
 		function updateTodo() {
 			todos.value.splice(selectedIndex.value, 1, {...todo.value})
 			isEditing.value = false
 			todo.value.itemName = ''
 		}
-		function deleteTodo(index) {
-			todos.value.splice(index, 1)
-		}
+
 		return {
 			isEditing,
 			selectedIndex,
 			todo,
 			todos,
 			addTodo,
-			editTodo,
-			done,
 			updateTodo,
+			editTodo,
 			deleteTodo
-
 		}
 	},
 }  
---
+++ src/components/TodoContent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="todolist_content" >
+		<p class="todolist_content_text" v-bind:class="{strikethrough:todo.status==='done'}">{{todo.itemName}}</p>
+		<button class="todolist_content_btn edit" @click="editTodo(index, todo)" >EDIT</button>
+		<button class="todolist_content_btn done" @click="done(index, todo)">DONE</button>
+		<button class="todolist_content_btn delete" @click="deleteTodo(index)">DELETE</button>	
+	</div>
+</template>
+
+<script>
+export default {
+    name: 'TodoContent',
+    props: {                           
+        todo: {
+            type: Object,
+            required: true,
+            default: () => ({}) 
+            
+        },
+        index: {
+            type: Number,    // type number - потому что число
+            required: true, 
+            default: 0   
+        }       
+    },
+    emits:[
+        'edit',
+        'done',
+        'delete'
+    ],
+    setup(props, {emit}) {
+        function editTodo(index, item) {
+            emit('edit', index, item)    // 'имя' передаю index и item
+            }
+        function done(index, todo) { // дз сделать кнопку            
+			todo.status='done'
+            emit('done')
+		}
+        function deleteTodo(index) {
+            emit('delete', index) // 'имя' передаю index
+		}
+        return {
+            editTodo,
+            done,
+            deleteTodo
+        }
+    }
+}
+</script>
+
+
