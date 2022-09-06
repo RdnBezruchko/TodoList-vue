85abe613adaa424a09b1c1eec3595f250e43358e
Rodion <drugoklasnik@gmail.com>
2022-09-06 14:01:44 +0300

    todo final

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -2,18 +2,18 @@
 	<div class="center">
 		<div class="wrapper" id="tasklist">  
 			<h1 class="todolist_header">ToDo List</h1>
-		<div v-if="!isEditing" class="todolist_subheader">
-			<input class="todolist_placeholder" type="text" placeholder="Add TO DO" v-model="todo.itemName"  @keyup.enter="addTodo">
-			<button class="todolist_btn add" value="add" @click="addTodo">ADD</button>
-		</div>
-		<div v-else class="todolist_subheader">
-			<input class="todolist_placeholder" type="text" placeholder="Add TO DO" v-model="todo.itemName" >
-			<button class="todolist_btn update" value="update" @click="updateTodo">UPDATE</button>
-		</div>
-		<div class="todolist_descr">
-			<div class="task">Task</div>
-			<div class="action">Action</div>
-		</div>
+			<div v-if="!isEditing" class="todolist_subheader">
+				<input class="todolist_placeholder" type="text" placeholder="Add TO DO" v-model="todo.itemName"  @keyup.enter="addTodo">
+				<button class="todolist_btn add" value="add" @click="addTodo">ADD</button>
+			</div>
+			<div v-else class="todolist_subheader">
+				<input class="todolist_placeholder" type="text" placeholder="Add TO DO" v-model="todo.itemName" >
+				<button class="todolist_btn update" value="update" @click="updateTodo(index)">UPDATE</button>
+			</div>
+			<div class="todolist_descr">
+				<div class="task">Task</div>
+				<div class="action">Action</div>
+			</div>
 		<!-- <ul class="todolist_content">
 			<li>
 				<div class="todolist_content_text" v-for="todo in todos" :key="todo">{{todo}}</div>
@@ -30,6 +30,7 @@
 		</ul> -->
 		<!-- <h1 v-for="todo in todos" :key="todo">{{todo}}</h1> -->
 		<TodoContent 
+
 		v-for="(todo, index) in todos"  
 		:key="index" 
 		:todo="todo"
@@ -48,6 +49,8 @@
 		/> 
 		-->
 		<!-- @done="done" -->
+	
+
 	</div>
 	</div>
 </template>
@@ -63,6 +66,11 @@ export default {
   	name: 'To do list',
 	components: {TodoContent},
 	setup(){
+		fetch('https://item-management.azurewebsites.net/api/items') // get запрос
+		.then(response => response.json())
+		.then(todo => todos.value = todo)
+		 .then(json => console.log(json))
+		// .then(json => { todos.value = json }) - выводит контент на экран
 		const isEditing = ref(false)
 		const selectedIndex = ref(null)
 		const todo = ref({
@@ -71,35 +79,72 @@ export default {
 			isStrikeThrough: false
 		})
 		const todos = ref([])
-		function deleteTodo(index) {
+
+
+		function deleteTodo(index, id) {
 			todos.value.splice(index, 1)
+			const deleteRequest = {
+			method: 'DELETE',
+			headers: {
+			'Content-type': 'application/json; charset=UTF-8' 
+			}}
+
+			fetch("https://item-management.azurewebsites.net/api/items/" + id, deleteRequest) 
+
+			.then(response => response.json())
+			.then(data => console.log(data)) 
+
 		}
 		function editTodo(index, item) {
 			todo.value = item
 			selectedIndex.value = index
 			isEditing.value = true
+
+			// const putRequest = {
+			// 	method: "PUT",
+			// 	headers: { "Content-Type": "application/json" },
+			// 	body: JSON.stringify(todo.value)
+  			// }		
+			// console.log(JSON.stringify(todo.value))
+  			// fetch("https://item-management.azurewebsites.net/api/items/" + todos.value[index].id, putRequest)
+			
 			// console.log(index, item)
+
 		}
+
 		function addTodo() {
 			if (todo.value.itemName.length === 0) {
 				return
 			}
 			todos.value.push({itemName:todo.value.itemName, dateTimeUtc: (new Date()).toUTCString()})
 			todo.value.dateTimeUtc = new Date().toISOString()
-			const requestOptions = {
+			const postRequest = {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(todo)
-  			};
-			
-			// console.log(JSON.stringify(this.todo))
-  			// fetch("https://item-management.azurewebsites.net/api/items", requestOptions)
+				body: JSON.stringify(todo.value)
+  			}		
+			console.log(JSON.stringify(todo.value))
+  			fetch("https://item-management.azurewebsites.net/api/items", postRequest)
+			.then(response => response.json(window.location.reload()))
 			todo.value.itemName = ''
-		}		
+			
+		}	
+
 		function updateTodo() {
 			todos.value.splice(selectedIndex.value, 1, {...todo.value})
 			isEditing.value = false
+			
+
+			const putOptions = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(todo.value)
+			};
+			fetch("https://item-management.azurewebsites.net/api/items/", putOptions)
+			.then(response => response.json())
+			console.log(JSON.stringify(todo.value))
 			todo.value.itemName = ''
+
 		}
 
 		return {
@@ -113,5 +158,6 @@ export default {
 			deleteTodo
 		}
 	},
+	
 }  
 </script>
--- src/components/TodoContent.vue
+++ src/components/TodoContent.vue
@@ -2,8 +2,8 @@
     <div class="todolist_content" >
 		<p class="todolist_content_text" v-bind:class="{strikethrough:todo.isStrikeThrough}">{{todo.itemName}}</p>
 		<button class="todolist_content_btn edit" @click="editTodo(index, todo)">EDIT</button>
-		<button class="todolist_content_btn done" @click="done(index, todo)">DONE</button>
-		<button class="todolist_content_btn delete" @click="deleteTodo(index)">DELETE</button>	
+		<button class="todolist_content_btn done" @click="done(index, todo, todo.id)">DONE</button>
+		<button class="todolist_content_btn delete" @click="deleteTodo(index, todo.id)">DELETE</button>	
 	</div>
 </template>
 
@@ -20,7 +20,8 @@ export default {
             type: Number,    // type number - потому что число
             required: true, 
             default: 0   
-        }       
+        }
+
     },
     emits:[
         'edit',
@@ -31,16 +32,16 @@ export default {
         function editTodo(index, item) {
             emit('edit', index, item)    // 'имя', передаю index и item
             }
-        function done(index, todo) {          
+        function done(index, todo, id) {          
 			todo.isStrikeThrough = !todo.isStrikeThrough
             console.log(todo.isStrikeThrough)         
             if (todo.isStrikeThrough === false) {
-                deleteTodo(index)
+                deleteTodo(index, id)   
             } 
             emit('done', todo)
-		}
-        function deleteTodo(index) {
-            emit('delete', index) // 'имя', передаю index
+		}   
+        function deleteTodo(index, id) {
+            emit('delete', index, id) // 'имя', передаю index
 		}
         return {
             editTodo,
